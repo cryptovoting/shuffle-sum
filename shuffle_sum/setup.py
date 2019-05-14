d90439505e36c69037638b949e6dcf7198fc3fd4
Kyle Swanson <swansonk.14@gmail.com>
2019-05-14 00:26:56 -0400

    Adding license

--- shuffle_sum/setup.py
+++ shuffle_sum/setup.py
@@ -1,11 +1,20 @@
+#!/usr/bin/env python3
+"""
+setup.py
+Boucher, Govedič, Saowakon, Swanson 2019
+
+Setup script for installation.
+
+"""
 import setuptools
+
 
 setuptools.setup(
     name="shuffle-sum",
     version="0.0.1",
     author="Boucher, Govedič, Saowakon, Swanson",
     description="Tallying of single transferable vote ballots using the Shuffle-Sum protocol.",
-    long_description="Tallying of single transferable vote ballots using the Shuffle-Sum protocol.",
+    long_description_content_type="Tallying of single transferable vote ballots using the Shuffle-Sum protocol.",
     url="https://github.com/cryptovoting/shuffle-sum",
     packages=setuptools.find_packages(),
     install_requires=[
@@ -15,6 +24,7 @@ setuptools.setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ]
 )
